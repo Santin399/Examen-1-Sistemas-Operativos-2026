02d54e0b000800ebed0588435664649a10e43940
Emiliano Santin <0274541@up.edu.mx>
2026-02-26 14:21:38 -0600

    pregunta 5 history

---
+++ pregunta5.sh
@@ -0,0 +1,76 @@
+    1  sudo apt update
+    2  sudo apt upgrade
+    3  vim
+    4  sudo apt install vim
+    5  vim
+    6  clear
+    7  ls
+    8  sudo apt install cmake
+    9  python
+   10  sudo apt install vscode
+   11  cd Downloads/
+   12  ls
+   13  sudo apt install code_1.108.1-1768404234_amd64.deb
+   14  sudo apt install ./code_1.108.1-1768404234_amd64.deb 
+   15  ls
+   16  cd Music
+   17  ls
+   18  cd
+   19  cd Desktop
+   20  ls
+   21  clear
+   22  cd
+   23  sudo dpkg-reconfigure keyboard-configuration
+   24  clear
+   25  sudo dpkg-reconfigure keyboard-configuration
+   26  cd Desktop
+   27  git config -- global user.name Santin399
+   28  ls
+   29  git config --global user.name Santin399
+   30  git config --global user.email 0274541@up.edu.mx
+   31  git config --global --list
+   32  git init
+   33  git add.
+   34  git add .
+   35  git commit -m 
+   36  git commit -m "initial commit: mini shell"
+   37  git branch -M main
+   38  git remote add origin https://github.com/santin399/Proyecto-Sistemas-Operativos-2026.git
+   39  git push -u origin main
+   40  clear
+   41  git config --global --list
+   42  git push -u origin main
+   43  install git
+   44  help
+   45  git install
+   46  sudo apt install git
+   47  sudo apt install g++
+   48  cd Desktop
+   49  nano main.cpp
+   50  clear
+   51  g++ -std=c++17 -Wall -Wextra main.cpp -o main
+   52  nano main.cpp
+   53  cd Desktop
+   54  g++ -std=c++17 -Wall -Wextra main.cpp -o main
+   55  cd desktop
+   56  cd Desktop
+   57  nano main.cpp
+   58  g++ -std=c++17 -Wall -Wextra main.cpp -o main
+   59  cd Descktop
+   60  cd Desktop
+   61  g++ -std=c++17 -Wall -Wextra main.cpp -o main
+   62  nano main.cpp
+   63  cd Descktop
+   64  cd Desktop
+   65  nano main
+   66  cd Desktop
+   67  nano main.cpp
+   68  cd Desktop
+   69  g++ -std=c++17 -Wall -Wextra main.cpp -o main
+   70  ./main
+   71  cd Desktop
+   72  ./main
+   73  /.main
+   74  ./main
+   75  touch accept.txt && ls > accept.txt && history | grep -w ls | wc -l >> accept.txt
+   76  history > pregunta5.sh
